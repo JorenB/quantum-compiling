f1c4e41d5827590ef8247b3d7d114565cc5df566
Joren Brunekreef <joren@dznd.nl>
2013-03-21 13:14:29 +0000

    added comments

--- gateSearch.m
+++ gateSearch.m
@@ -1,6 +1,9 @@
+%recursive function for computing matrix product in tree up to specific depth.
+%if a previously encountered matrix is calculated, the algorithm exits the branch and backtracks in order to generate new matrices
 function b = gateSearch(search, depth, precision, current, path, history)
 	b = {};
-
+	
+	%retrieve base matrices
 	matrices = braidMatrices();
 
 	for j = 1:length(matrices)
@@ -18,23 +21,28 @@ function b = gateSearch(search, depth, precision, current, path, history)
 			end
 		end
 
+		%current matrix has already appeared somewhere - walking this branch any further is useless
 		if c
 			continue;
 		end	
+
 		history{length(history)+1} = test;
 
 		currentPath = path;
-			currentPath{length(currentPath)+1} = j;
-			b{length(b)+1} = currentPath;
+		currentPath{length(currentPath)+1} = j;
+		b{length(b)+1} = currentPath;
 
-			if norm(test(1:2, 1:2) - refGates(search)) < precision 
-				%disp(test);
-				%disp(currentPath);
-				%disp(norm(test(1:2, 1:2) - refGates(search))); 
-			end
+		%if desired gate has been found within specified precision, display it
+		if norm(test(1:2, 1:2) - refGates(search)) < precision 
+			%disp(test);
+			%disp(currentPath);
+			%disp(norm(test(1:2, 1:2) - refGates(search))); 
+		end
 
-			if depth > 1 
-				b = [ b gateSearch(search, depth-1, precision, test, currentPath, history)];
-			end
+		%halt if depth limit has been reached
+		if depth > 1 
+			b = [ b gateSearch(search, depth-1, precision, test, currentPath, history)];
+		end
+
 	end
 end

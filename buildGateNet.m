381647d4cdc369fce1d02cccbecc735a8d9a0fbd
Joren Brunekreef <joren@dznd.nl>
2013-03-25 14:27:44 +0000

    minor optimizations, profiling and comment updates

--- buildGateNet.m
+++ buildGateNet.m
@@ -3,21 +3,27 @@
 function gates = buildGateNet(depth, tightness, base, collection, net)
 	gates = collection;
 	
+	disp('---');
+	disp(depth);
+	disp(size(collection,2));
+
 	new_gates = {};
+	t = tic;
 	for j = 1:length(base)
         for k = 1:length(collection)
             candidate = base{j} * collection{k}; 
 
             % check whether gate already appeared in history
             unique = true;
-            for l = 1:length(net)
-                if norm(candidate-net{l}) < tightness
+			ln = length(net);
+            for l = 1:ln
+                if norm(candidate-net{l}, 2) < tightness
                     unique = false;
                     break;
                 end
             end
 
-            %current matrix has already appeared somewhere - walking this branch any further is useless
+            %candidate matrix is unique so far: add it to the list
             if unique == true
                 new_gates{length(new_gates)+1} = candidate;
                 net{length(net)+1} = candidate;
@@ -25,8 +31,10 @@ function gates = buildGateNet(depth, tightness, base, collection, net)
         end
 	end
 
+	disp(toc(t));
+
 	%halt if depth limit has been reached
-    if depth > 1  
+    if depth > 1 && size(new_gates, 2) > 0
 		new_gates = buildGateNet(depth - 1, tightness, base, new_gates, net);
 	end
     
--- time_netbuild.m
+++ time_netbuild.m
@@ -1,10 +1,6 @@
 function b = time_netbuild(depth, tightness)  
-if nargin < 1
-	depth = 6;
-end
-
-
 s = tic;
-b = buildGateNet(depth, tightness, constants.MATRICES, {eye(3)}, {eye(3)});
+dimension = size(constants.MATRICES{1}, 1);
+b = buildGateNet(depth, tightness, constants.MATRICES, {eye(dimension)}, {eye(dimension)});
 toc(s)
 end

81a8ee204eca70b87f367ec6fa5ac5bd9cfcd0d5
Joren Brunekreef <joren@dznd.nl>
2013-04-25 17:44:01 +0100

    rewrote gate-tree walking algorithm to a more natural output

--- buildGateNet.m
+++ buildGateNet.m
@@ -1,44 +1,48 @@
 %recursive function for computing matrix product in tree up to specific depth.
 %if a previously encountered matrix is calculated, the algorithm exits the branch and backtracks in order to generate new matrices
-function gates = buildGateNet(depth, base, collection, net)
-	gates = collection;
-	
+function [total_gates, total_words] = buildGateNet(depth, base, upper_gates, upper_words, full_gates)
+
 	disp('---');
 	disp(depth);
-	disp(size(collection,2));
+	disp(size(upper_gates,2));
 
 	new_gates = {};
+	new_words = {};
 	t = tic;
+	
+
 	for j = 1:length(base)
-        for k = 1:length(collection)
-            candidate = base{j} * collection{k}{1}; 
+        for k = 1:length(upper_gates)
+            candidate = base{j} * upper_gates{k}; 
 
             % check whether gate already appeared in history
             unique = true;
-			ln = length(net);
-            for l = 1:ln
+            for l = 1:length(full_gates)
 				%n = traceDistance(candidate,net{l});
-				n = norm(candidate - net{l});
+				n = norm(candidate - full_gates{l});
                 if n < constants.RE
                     unique = false;
+					candidate;
                     break;
                 end
             end
 
             %candidate matrix is unique so far: add it to the list
             if unique == true
-                new_gates{length(new_gates)+1} = {candidate, [int2str(j), collection{k}{2} ]};
-                net{length(net)+1} = candidate;
+                new_gates{length(new_gates)+1} = candidate;
+				new_words{length(new_words)+1} = [int2str(j), upper_words{k} ];
             end
         end
 	end
 
 	disp(toc(t));
 
+	full_gates = [ full_gates upper_gates ];
 	%halt if depth limit has been reached
     if depth > 1 && size(new_gates, 2) > 0
-		new_gates = buildGateNet(depth - 1, base, new_gates, net);
+		[new_gates new_words] = buildGateNet(depth - 1, base, new_gates, new_words, full_gates);
 	end
     
-	gates = [ gates new_gates ];
+	total_gates = [ upper_gates new_gates ];
+	total_words = [ upper_words new_words ];
 end
--- time_netbuild.m
+++ time_netbuild.m
@@ -1,6 +1,6 @@
-function b = time_netbuild(depth)  
+function [gates words] = time_netbuild(depth)  
 s = tic;
 dimension = size(constants.MATRICES{1}, 1);
-b = buildGateNet(depth, constants.MATRICES, {{eye(dimension), ''}}, {eye(dimension)});
+[gates words] = buildGateNet(depth, constants.MATRICES, {eye(dimension)}, {''}, {eye(dimension)});
 toc(s)
 end
